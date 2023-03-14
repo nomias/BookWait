6747ffb181c27bee4ef49747a4e83b2879115e2a
Saimon Shrestha <sai.shrestha14@gmail.com>
2023-03-14 11:25:36 -0400

    bookAdd adds books to bookList correctly

--- bookAdd.py
+++ bookAdd.py
@@ -1,20 +1,13 @@
 import csv
-from book import Book
 
-books=[]
+books=[["Name","ISBN"]]
 while True:
     bookName=input("Book Name: ")
     if bookName=="false":
         break
     bookISBN=input("Book ISBN: ")
-    book=Book(bookName,bookISBN)
+    book=[bookName,bookISBN]
     books.append(book)
-book=books[0]
-print(book)
-fields=["Name","ISBN"]
 with open('bookList.csv', 'w', newline='') as file: 
-    writer = csv.DictWriter(file, fieldnames = fields)
-    writer.writeheader()
-    #for book in books:
-     #   writer.writerow(book)
-#f=open("bookList.csv","w")
+    writer=csv.writer(file)
+    writer.writerows(books)
