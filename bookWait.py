6d4d1e80ad12acccdf7011f9c5cecf6a0547e0fd
Saimon Shrestha <sai.shrestha14@gmail.com>
2023-03-14 10:06:28 -0400

    site access and testing to see availability done

---
+++ bookWait.py
@@ -0,0 +1,14 @@
+import requests
+import smtplib
+from bs4 import BeautifulSoup
+
+headers={
+    "User-agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69'
+}
+URL='https://www.barnesandnoble.com/w/spy-x-family-vol-9-tatsuya-endo/1141652264?ean=9781974736287'
+page=requests.get(URL,headers=headers)
+#print(page)
+
+soup=BeautifulSoup(page.text,'html.parser')
+val=soup.find("input",value="ADD TO CART")
+print(val)
